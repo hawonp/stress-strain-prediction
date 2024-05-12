31469ac730b12e16b3db854320b9fda3660ca84a
hawonp <hawonp@gmail.com>
2024-05-12 20:50:53 +0900

    fix preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,6 @@
-from preprocess.generate_squares import generate_square_images
 from preprocess.preprocess_circles import apply_material_properties  # noqa
+from preprocess.preprocess_squares import generate_square_images
 
 if __name__ == "__main__":
-    # apply_material_properties()
+    apply_material_properties()
     generate_square_images()
